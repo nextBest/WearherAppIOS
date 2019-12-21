42546c9c2b60dd2a8ed7471f5b32cddffa541cc5
nextbest <skalkowskimateusz@gmial.com>
2019-12-21 21:53:04 +0100

    crate project

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Mateusz Skałkowski on 21/12/2019.
+//  Copyright © 2019 Mateusz Skałkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
