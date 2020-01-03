683bcf37183f5b0e5edb55ab700e2e46e0ad227b
Mateusz Skalkowski <msklakowski@concisesoftware.com>
2020-01-03 10:47:46 +0100

    create storyboards

---
+++ WeatherApp/WeatherApp/Storyboards/Storyboard.swift
@@ -0,0 +1,11 @@
+//
+//  Storyboard.swift
+//  WeatherApp
+//
+//  Created by Mateusz Skalkowski on 03/01/2020.
+//  Copyright © 2020 Mateusz Skałkowski. All rights reserved.
+//
+
+enum Storyboard: String {
+    case dashboard = "Dashboard"
+}
