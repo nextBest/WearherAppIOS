a921e14c8caaa6c2f4f03e4c665c96f60a607f91
Mateusz Skalkowski <msklakowski@concisesoftware.com>
2020-01-08 09:56:03 +0100

    create request manager

--- WeatherApp/WeatherApp/Data/Network/RequestManager.swift
+++ WeatherApp/WeatherApp/Data/Network/RequestManager.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import Alamofire
+
+class RequestManager {
+    private let sessionManager = Session()
+    
+    func makeRequest<T>(router: ApiRouter, resultType: T.Type, success: @escaping (T) -> Void, fail: @escaping (AFError) -> Void) where T: Codable {
+        // TODO add interceptor
+        sessionManager.request(router, interceptor: nil).validate().response { (response) in
+            switch response.result {
+            case .success(let reponseData):
+                guard let data = reponseData else {
+                    return
+                }
+                
+                do {
+                    let data = try JSONDecoder().decode(resultType, from: data)
+                    success(data)
+                } catch let error {
+                    print("===Parse error===", error)
+                    
+                }
+            case .failure(let error):
+                fail(error)
+            }
+        }
+    }
+}
