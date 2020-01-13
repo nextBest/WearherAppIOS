f43972a8e3a2c43db2e434d47deb246b3008cd58
Mateusz Skalkowski <msklakowski@concisesoftware.com>
2020-01-13 10:02:33 +0100

    add models

---
+++ WeatherApp/WeatherApp/Data/Model/Location.swift
@@ -0,0 +1,25 @@
+//
+//  Location.swift
+//  WeatherApp
+//
+//  Created by Mateusz Skalkowski on 10/01/2020.
+//  Copyright © 2020 Mateusz Skałkowski. All rights reserved.
+//
+
+import Foundation
+
+struct Location: Codable {
+    let distance: Int
+    let title: String
+    let locationType: String
+    let woeid: Int
+    let lattLong: String
+
+    enum CodingKeys: String, CodingKey {
+        case distance, title, woeid
+        case locationType = "location_type"
+        case lattLong = "latt_long"
+    }
+}
+
+typealias LocationList = [Location]
---
+++ WeatherApp/WeatherApp/Data/Model/LocationTextParams.swift
@@ -0,0 +1,13 @@
+//
+//  LocationTextParams.swift
+//  WeatherApp
+//
+//  Created by Mateusz Skalkowski on 10/01/2020.
+//  Copyright © 2020 Mateusz Skałkowski. All rights reserved.
+//
+
+import Foundation
+
+struct LocationTextParams: Encodable {
+    var query: String
+}
