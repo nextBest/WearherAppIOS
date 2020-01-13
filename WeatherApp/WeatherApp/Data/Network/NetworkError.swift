50370e0fb38730bc239d90e786f368936d115d57
Mateusz Skalkowski <msklakowski@concisesoftware.com>
2020-01-13 16:13:55 +0100

    add network error enum

---
+++ WeatherApp/WeatherApp/Data/Network/NetworkError.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkError.swift
+//  WeatherApp
+//
+//  Created by Mateusz Skalkowski on 13/01/2020.
+//  Copyright © 2020 Mateusz Skałkowski. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case connectionError
+    case parseError
+    case defaultError
+}
