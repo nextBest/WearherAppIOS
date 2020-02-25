46b6f5be009d668d21de0812761d94eb525edab3
Mateusz Skalkowski <msklakowski@concisesoftware.com>
2020-02-25 09:41:01 +0100

    weather forecast view

---
+++ WeatherApp/WeatherApp/Screens/DetailsFlow/WeatherDetails/View/ForecastView/ForecastView.swift
@@ -0,0 +1,25 @@
+//
+//  ForecastView.swift
+//  WeatherApp
+//
+//  Created by Mateusz Skalkowski on 03/02/2020.
+//  Copyright © 2020 Mateusz Skałkowski. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class ForecastView: CustomXibView {
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var tempDayLabel: UILabel!
+    @IBOutlet weak var tempNightLabel: UILabel!
+    
+    func setup(weather: Weather) {
+        dayLabel.text = weather.applicableDate.dateToDay()
+        weatherImage.sd_setImage(with: URL(string: "https://www.metaweather.com/static/img/weather/png/\(weather.weatherStateAbbr).png"), completed: nil)
+        tempDayLabel.text = weather.maxTemp.temperature()
+        tempNightLabel.text = weather.minTemp.temperature()
+    }
+}
