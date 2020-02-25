80a29cbb3c18c47b4c3577a1d324d349ba2cbf3a
Mateusz Skalkowski <msklakowski@concisesoftware.com>
2020-02-25 14:05:01 +0100

    create weather time table view cell

---
+++ WeatherApp/WeatherApp/Screens/DetailsFlow/WeatherDetails/View/WeatherTimeTableViewCell/WeatherTimeTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherTimeTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Mateusz Skalkowski on 25/02/2020.
+//  Copyright © 2020 Mateusz Skałkowski. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTimeTableViewCell: UITableViewCell {
+    
+    
+}
